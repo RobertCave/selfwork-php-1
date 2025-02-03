309b6a2ac2ce73866f91f222b162c81af3d1ab60
Roberto Lacava <roberto.lacava@gmail.com>
2025-02-03 14:11:12 +0100

    first commit

---
+++ php_01_roberto_lacava.php
@@ -0,0 +1,52 @@
+<?php
+// Traccia 1:
+
+// Installare PHP
+// Definisci 4 variabili:
+//     Integer
+//     Float
+//     String
+//     Boolean
+// A schermo, fai comparire il tipo di dato delle varie variabili.
+// Trasforma quelle variabili in costanti, utilizzando le best practice viste a lezione.
+// Pusha il codice su GitHub con il nome php_01_nome_cognome. Integra anche le altre tracce e poi pusha tutto.
+
+
+
+// Variabile Integer
+$integerVar = 42;
+
+// Variabile Float
+$floatVar = 3.14;
+
+// Variabile String
+$stringVar = "Ciao mondo!";
+
+// Variabile Boolean
+$boolVar = true;
+ 
+
+var_dump($integerVar); // Tipo di integer;
+var_dump($floatVar); // Tipo di float
+var_dump($stringVar); //Tipo di string
+var_dump($boolVar); //Tipo di bool
+ 
+
+
+// Costante Integer
+const INTEGERVAR = 42;
+
+// Costante Float
+const FLOATVAR = 3.14; 
+
+// Costante String
+const STRINGVAR= "Ciao mondo!";
+ 
+// Costante Boolean
+const BOOLVAR = true; 
+
+
+
+
+
+?>
